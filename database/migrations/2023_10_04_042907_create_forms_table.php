f48ced0cc04facbe0f71dc8be030bcd90abcd81d
Sherxon <116145014+SherxonBoboyorov@users.noreply.github.com>
2023-10-04 09:36:47 +0500

    Some changes

---
+++ database/migrations/2023_10_04_042907_create_forms_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('pursue')->nullable();
+
+            $table->string('names');
+            $table->string('gmail');
+            $table->string('numbers');
+
+            $table->text('content')->nullable();
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forms');
+    }
+};
